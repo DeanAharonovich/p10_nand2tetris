ae8a6b13f52b5a37258d9f5e24c47406096348fd
Dean Aharonovich <dean.aha96@gmail.com>
2023-01-14 17:08:56 +0200

    CompilationEngine update

--- CompilationEngine.py
+++ CompilationEngine.py
@@ -1,15 +1,19 @@
-class CompilationEngine:
-    def write_xml_key(expression):
-        getattr("compile_", expression)()
-    def write_xml_key(expression):
-        getattr("compile_", expression)()
-    def write_xml_key(expression):
-        getattr("compile_", expression)()
-    def write_xml_key(expression):
-        getattr("compile_", expression)()
-    def write_xml_key(expression):
-        getattr("compile_", expression)()
+from JackTokenaizer import JackTokenaizer
 
-    def compile_class():
-        xml = "<keyword> class </keyword>"
+class CompilationEngine:
+    def __init__(self, token: JackTokenaizer):
+        self.current_token = token.current_token
+    
+    def compile_if(self):
+        xml = "<ifStatement>\n"
+        xml += self.process("if") + self.process("(") + self.compile_expression() \
+            + self.process(")") + self.process("\{") + self.compile_statements() + self.process("\}")
+        xml += "</ifStatement>\n"
         return xml
+
+    def process(self ,str):
+        if (str == self.current_token):
+            xml = "<keyword> {} </keyword>\n".format(str)
+        else:
+            xml = "SYNTAX ERROR"
+        self.current_token.advance()
